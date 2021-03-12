2678cdab2d710fb500906f36691c180630a06983
Ale Bausa <ale.bausa@gmail.com>
2021-03-12 09:22:41 +0100

    Adds enemy spawn logic

--- js/index.js
+++ js/index.js
@@ -27,6 +27,36 @@ window.addEventListener('click', (event) => {
 })
 
 // Create enemies and spawn them towards player
+const enemies = [];
+function spawnEenemies(){
+  setInterval(() => {
+  // Where they go
+  let x;
+  let y;
+  const radius = Math.random() * (30 - 4) + 4;
+  if (Math.random() < 0.5){
+    x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+    y = Math.random() * canvas.height;
+  } else {
+    x = Math.random() * canvas.width;
+    y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+  }
+  const angle = Math.atan2(canvas.width / 2 - y, canvas.width/2 - x);
+  const color = "green";
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)}
+  enemies.push(new Enemy(
+   x,
+   y,
+   radius,
+   color,
+   velocity
+ ))
+  }, 1000);
+}
+
+spawnEenemies();
 
 // Clean the screen
 function _clean(){
@@ -38,6 +68,7 @@ function animate(){
   _clean();
   player._draw();
   projectiles.forEach(projectile => projectile._update());
+  enemies.forEach(enemy => enemy._update());
   window.requestAnimationFrame(animate);
 }
 
